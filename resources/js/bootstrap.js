e9f190ce82375b59c74a1b28a09aea1e7c6dc037
mouheb1 <mouheb.bzri@gmail.com>
2025-01-14 09:03:34 +0100

    comment x-csrf-token in bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,7 +3,7 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-// Set CSRF token for Axios
-axios.defaults.headers.common['X-CSRF-TOKEN'] = document
-    .querySelector('meta[name="csrf-token"]')
-    .getAttribute('content');
+// // Set CSRF token for Axios
+// axios.defaults.headers.common['X-CSRF-TOKEN'] = document
+//     .querySelector('meta[name="csrf-token"]')
+//     .getAttribute('content');
